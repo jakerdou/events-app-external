1018557460fbde4495d17a2afe86bf1135082fdc
00tpotter <00tpotter@gmail.com>
2022-08-17 20:38:33 +0000

    Teddy committing

--- test/test-server.js
+++ test/test-server.js
@@ -5,7 +5,7 @@ const app = require('../server');
 
 describe('GET /', function () {
   it('responds with home page', function (done) {
-
+    // Adding stuff
     //specify the url to be intercepted
     nock("http://localhost:8082")
       //define the method to be intercepted
